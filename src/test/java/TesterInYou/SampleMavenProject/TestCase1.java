68cd9372acd02ed81c5d3f2465d2eb6808dc706d
danishgit <danishfaisal04@gmail.com>
2015-09-06 15:32:03 +0530

    Updated a file

--- src/test/java/TesterInYou/SampleMavenProject/TestCase1.java
+++ src/test/java/TesterInYou/SampleMavenProject/TestCase1.java
@@ -16,8 +16,8 @@ public class TestCase1 {
 	  a.moveToElement(driver.findElement(By.xpath("//li[@class='level0 nav-3 level-top parent']/a"))).perform();
 	  a.moveToElement(driver.findElement(By.linkText("Furniture "))).perform();
 	  a.moveToElement(driver.findElement(By.linkText(" Vanities & Desks "))).perform();
-	  driver.findElement(By.linkText(" Vanities & Desks ")).click();
 	  System.out.println("Thi is for testing");
+	  System.out.println("This if for git commit");
   }
   @BeforeClass
   public void beforeClass() {
